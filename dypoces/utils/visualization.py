09a35b3febb81a8552071d069126e8108c20e6ca
bo <aliosmanberk@sabanciuniv.edu>
2021-08-06 16:39:29 +0300

    feat: new simulation and report script

--- dypoces/utils/visualization.py
+++ dypoces/utils/visualization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import networkx as nx
 
 
@@ -19,5 +20,26 @@ class nxDraw:
             plt.savefig(path)
 
 
+def catplot_results(df, x, y, hue, col, row, title):
+    sns.set_theme(style="whitegrid")
+    g = sns.catplot(
+        data=df,
+        x=x,
+        y=y,
+        hue=hue,
+        col=col,
+        row=row,
+        palette="hls",
+        capsize=0.2,
+        height=6,
+        aspect=0.75,
+        kind="point",
+        **kwargs
+    )
+    g.fig.suptitle(title)
+    g.despine(left=True)
+    return g
+
+
 if __name__ == "__main__":
-    n = 500
+    pass
