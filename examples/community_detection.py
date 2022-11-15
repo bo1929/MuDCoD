2a9749c319451c766dea6e72429feeec184f81a9
bo1929 <asapci@ucsd.edu>
2022-11-14 18:16:16 -0800

    Updated examples.

--- examples/community_detection.py
+++ examples/community_detection.py
@@ -1,65 +1,64 @@
-# One easy example for MuSPCES.
-import sys
 import numpy as np
+from mudcod.community_detection import MuDCoD, StaticSpectralCoD
+from mudcod.network_simulations import MuSDynamicDCBM
+from sklearn.metrics.cluster import adjusted_rand_score
 
-from sklearn.metrics.cluster import adjusted_rand_score  # noqa: E402
+T = 4
+S = 8
 
-sys.path.append("../")
-
-from mudcod.dcbm import MuSDynamicDCBM  # noqa: E402
-from mudcod.muspces import MuSPCES  # noqa: E402
-from mudcod.static import Static  # noqa: E402
-
+# Multi-subject Dynamic DCBM
 model_dcbm = MuSDynamicDCBM(
-    n=100,  # Total number of vertices
-    k=2,  # Model order, i.e. number of class labels.
+    n=200,  # Total number of vertices
+    model_order_K=3,  # Model order, i.e. number of class labels.
     p_in=(0.2, 0.25),  # In class connectivity parameter
-    p_out=0.1,  # Out class connectivity parameter
-    time_horizon=4,  # Total number of time steps
+    p_out=(0.1, 0.1),  # Out class connectivity parameter
+    time_horizon=T,  # Total number of time steps
     r_time=0.2,  # Probability of changing class labels in the next time step
-    num_subjects=8,  # Total number of subjects
+    num_subjects=S,  # Total number of subjects
     r_subject=0.2,  # Probability of changing class labels while evolving
+    seed=0,
 )
-# scenario 3 (SSoS): strong signal sharing among subjects.
-adj_ms_series, z_true = model_dcbm.simulate_ms_dynamic_dcbm(scenario=3)
 
-# Initialize.
-muspces = MuSPCES(verbose=False)
+# setting 2 (SSoS): strong signal sharing among subjects.
+print("Setting SSoT: signal sharing over time, subjects evolve independently.")
+adj_mus_dynamic, z_mus_dynamic_true = model_dcbm.simulate_mus_dynamic_dcbm(setting=2)
 
 # Some reasonable hyper-parameters.
-alpha = 0.05 * np.ones((adj_ms_series.shape[1], 2))
-beta = 0.05 * np.ones(adj_ms_series.shape[0])
-k_max = 10
+alpha = 0.05 * np.ones((T, 2))
+beta = 0.05 * np.ones(S)
+max_K = 10
 n_iter = 30
 
-# Predict communities using MuDCoD.
-muspces.fit(
-    adj_ms_series[:, :, :],
+pred_MuDCoD = MuDCoD(verbose=False).fit_predict(
+    adj_mus_dynamic,
     alpha=alpha,
     beta=beta,
-    k_max=k_max,
+    max_K=max_K,
     n_iter=n_iter,
+    opt_K="null",
     monitor_convergence=True,
-    degree_correction=True,
 )
-z_pred_mudcod = muspces.predict()
-# Show convergence
-## print(f"Convergence of MuDCoD: {muspces.convergence_monitor}.")
 
-# Predict communities using static spectral clustering
-# seperately for each time step and subject
-static = Static(verbose=False)
-z_pred_static = np.empty_like(z_pred_mudcod)
+# Predict communities using static spectral clustering,
+# seperately for each time step and subject.
+pred_StaticSpectralCoD = np.empty_like(pred_MuDCoD)
 
-for sbj in range(adj_ms_series.shape[0]):
-    for th in range(adj_ms_series.shape[1]):
-        z_pred_static[sbj, th, :] = static.fit_predict(adj_ms_series[sbj, th, :, :])
+for sbj in range(adj_mus_dynamic.shape[0]):
+    for tp in range(adj_mus_dynamic.shape[1]):
+        pred_StaticSpectralCoD[sbj, tp, :] = StaticSpectralCoD(
+            verbose=False
+        ).fit_predict(adj_mus_dynamic[sbj, tp, :, :])
 
 # Compare mean adjusted Rand index scores.
-for sbj in range(adj_ms_series.shape[0]):
-    for th in range(adj_ms_series.shape[1]):
-        ari_mudcod = adjusted_rand_score(z_true[sbj, th, :], z_pred_mudcod[sbj, th, :])
-        ari_static = adjusted_rand_score(z_true[sbj, th, :], z_pred_static[sbj, th, :])
+ari_MuDCoD, ari_StaticSpectralCoD = 0, 0
+for sbj in range(adj_mus_dynamic.shape[0]):
+    for tp in range(adj_mus_dynamic.shape[1]):
+        ari_MuDCoD += adjusted_rand_score(
+            z_mus_dynamic_true[sbj, tp, :], pred_MuDCoD[sbj, tp, :]
+        )
+        ari_StaticSpectralCoD += adjusted_rand_score(
+            z_mus_dynamic_true[sbj, tp, :], pred_StaticSpectralCoD[sbj, tp, :]
+        )
 
-print(f"mean(ARI) of MuDCoD: {np.mean(ari_mudcod)}")
-print(f"mean(ARI) of static: {np.mean(ari_static)}")
+print(f"mean(ARI) of MuDCoD: {ari_MuDCoD/T/S}")
+print(f"mean(ARI) of StaticSpectralCoD: {ari_StaticSpectralCoD/T/S}")
